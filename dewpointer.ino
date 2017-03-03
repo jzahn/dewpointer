575c3887ece8992a21f11d5beee2ef3d4f1f7419
jzahn <smedge@gmail.com>
2017-03-03 11:32:01 -0600

    cleanup

--- dewpointer.ino
+++ dewpointer.ino
@@ -135,7 +135,7 @@ int read_lcd_buttons()
   adc_key_in = analogRead(0);      // read the value from the sensor
   // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
   // we add approx 50 to those values and check to see if we are close
-  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons
   if (adc_key_in < 50)   return btnRIGHT;
   if (adc_key_in < 195)  return btnUP;
   if (adc_key_in < 380)  return btnSELECT;
